c1ad5857614967c1654c8557f65d4f890d9611c7
Williams Isaac <williamscalg@gmail.com>
2020-03-10 19:03:15 +0100

    push error prone server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+require("@babel/polyfill/noConflict");
+require('@babel/register')({
+    presets: ['@babel/preset-env']
+});
+
+const hello = "HELLO";
+name = "John Doe";
+
+console.log({ hello, name, user });
